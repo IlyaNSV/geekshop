eb688adff1bfae569f8c6782042789d848ddcb90
Ilya Nosov <pw2013pw2013@mail.ru>
2020-06-18 21:32:21 +0600

    Урок 2 финальный. -Применён шаблон ко всем страницам сайта, всё работает хорошо -Прописан статический тег -Прописаны динамические ссылки -добавлен for при формировании каталога -добавлена динамика в дате в футере

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-
-
+from datetime import datetime
 # Create your views here.
+
+date = datetime.now()
 
 def index(request):
     context = {
         'page_title': 'Главная',
+        'year':date.year,
     }
     return render(request, 'mainapp/index.html', context)
 
@@ -13,12 +15,34 @@ def index(request):
 def contacts(request):
     context = {
         'page_title': 'Контакты',
+        'year':date.year,
     }
     return render(request, 'mainapp/contacts.html', context)
 
 
 def catalog(request):
+    products = [
+        {
+            'name':'Carlione',
+            'picture':'img/carlione.jpg',
+            'price':'10000'
+        },
+        {
+            'name': 'Palzileri',
+            'picture': 'img/palzileri.jpg',
+            'price': '12000'
+        },
+        {
+            'name': 'Parlamenter',
+            'picture': 'img/parlamenter.jpg',
+            'price': '15000'
+        },
+    ]
+
     context = {
         'page_title': 'Каталог',
+        'products': products,
+        'year':date.year,
     }
+
     return render(request, 'mainapp/catalog.html', context)
