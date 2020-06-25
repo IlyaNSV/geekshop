cb291b73075b247e72a1d06aa796e21ff3eedc94
Ilya Nosov <pw2013pw2013@mail.ru>
2020-06-25 23:56:14 +0600

    Урок 4 -authapp

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -6,6 +6,7 @@ import mainapp.views as mainapp
 
 urlpatterns = [
     path('', include('mainapp.urls', namespace='main')),
+    path('auth/', include('authapp.urls', namespace='auth')),
     path('admin/', admin.site.urls),
 ]
 
