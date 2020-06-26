03066fb706ae7482dffc6a7512cfa57cb3398bd5
Ilya Nosov <pw2013pw2013@mail.ru>
2020-06-26 22:40:24 +0600

    Урок 4 -authapp -filldb -ShopUser

--- authapp/models.py
+++ authapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User, AbstractUser
+
+class ShopUser(AbstractUser):
+    age = models.PositiveSmallIntegerField(verbose_name='возраст', null=True)
+    avatar = models.ImageField(upload_to='users_avatars', blank=True)
+
+
