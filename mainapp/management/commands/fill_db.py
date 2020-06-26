0927cc5af135d02209a7c4b00e3325b9eed9a5af
Ilya Nosov <pw2013pw2013@mail.ru>
2020-06-26 23:11:41 +0600

    Урок 4 -authapp -filldb (updated) -ShopUser

--- mainapp/management/commands/fill_db.py
+++ mainapp/management/commands/fill_db.py
@@ -3,18 +3,20 @@ import json
 
 from django.core.management.base import BaseCommand
 
-#from authapp.models import ShopUser
+from authapp.models import ShopUser
 from mainapp.models import ProductCategory, Product
 from django.conf import settings
 
 
+
+
+'''
 def load_from_json(file_name):
     with open(os.path.join(settings.JSON_PATH, f'{file_name}.json'),
               'r',
               encoding='utf-8'
     ) as infile:
         return json.load(infile)
-
 
 class Command(BaseCommand):
     help = 'Fill database with data'
@@ -34,4 +36,11 @@ class Command(BaseCommand):
             product['category']=_category
             new_category = Product(**product)
             new_category.save()
-
+            
+        if ShopUser.objects.filter(username='django').count() == 0:
+        ShopUser.objects.create(username='django',email='admin@geekshop.local',password='geekbrains')
+'''
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        if not ShopUser.objects.filter(username='django').exists():
+            ShopUser.objects.create_superuser(username='django', email='admin@geekshop.local', password='geekbrains')
