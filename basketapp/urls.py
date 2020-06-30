1a1d00607bc25fbf84550abbc72fdb51192535bd
Ilya Nosov <pw2013pw2013@mail.ru>
2020-07-01 00:11:40 +0600

    Lesson 5 - user register - user update - product categories - active link - basket - basket add

--- basketapp/urls.py
+++ basketapp/urls.py
@@ -5,5 +5,5 @@ import basketapp.views as basketapp
 app_name = 'basketapp'
 
 urlpatterns = [
-    #path('add/product/', basketapp.add_product, name='add_product'),
+    path('add/product/<int:pk>/', basketapp.add_product, name='add_product'),
 ]
