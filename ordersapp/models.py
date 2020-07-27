e5841b96ac0310d03a3e94df05c0abbcf1f71da5
Ilya Nosov <pw2013pw2013@mail.ru>
2020-07-27 13:39:54 +0600

    Django 2 Lesson 3     -dropdown     -ordersapp (index page)     -ordersapp full

--- ordersapp/models.py
+++ ordersapp/models.py
@@ -38,6 +38,25 @@ class Order(models.Model):
     def __str__(self):
         return f'Текущий заказ: {self.id}'
 
+    def get_total_quantity(self):
+        items = self.orderitems.all()
+        return sum(list(map(lambda x:x.quantity, items)))
+
+    def get_product_type_quantity(self):
+        items = self.orderitems.all()
+        return len(items)
+
+    def get_total_cost(self):
+        items = self.orderitems.all()
+        return sum(list(map(lambda x:x.quantity*x.product.price, items)))
+
+    # def delete(self):
+    #     for item in self.orderitems.all():
+    #         item.product.quantity += item.quantity
+    #         item.product.save()
+    #     self.is_active = False
+    #     self.save()
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name="orderitems",
--- ordersapp/urls.py
+++ ordersapp/urls.py
@@ -6,4 +6,10 @@ app_name = 'ordersapp'
 
 urlpatterns = [
     path('', ordersapp.OrderList.as_view(), name='index'),
+    path('order/create/', ordersapp.OrderCreate.as_view(), name='order_create'),
+    path('order/update/<int:pk>/', ordersapp.OrderUpdate.as_view(), name='order_update'),
+    path('order/delete/<int:pk>/', ordersapp.OrderDelete.as_view(), name='order_delete'),
+    path('order/read/<int:pk>/', ordersapp.OrderRead.as_view(), name='order_read'),
+    path('order/forming/complete/<int:pk>/', ordersapp.order_forming_complete, name='order_forming_complete'),
+
 ]
